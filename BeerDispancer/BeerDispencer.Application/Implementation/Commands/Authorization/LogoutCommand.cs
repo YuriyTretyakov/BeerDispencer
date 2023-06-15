1b2910e5245dc60d77693f00279c2caf899abf5b
Yurii Tretiakov <pokusun.yt@gmail.com>
2023-06-15 11:11:36 +0200

    logout command

---
+++ BeerDispancer/BeerDispencer.Application/Implementation/Commands/Authorization/LogoutCommand.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using BeerDispencer.Application.Implementation.Response;
+using MediatR;
+
+namespace BeerDispencer.Application.Implementation.Commands.Authorization
+{
+	public class LogoutCommand:IRequest<AuthResponseDto>
+	{
+		
+	}
+}
+
