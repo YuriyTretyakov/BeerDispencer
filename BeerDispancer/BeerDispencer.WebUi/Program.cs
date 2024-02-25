b99e8dbb92737033249b6bbc2de708f76f113981
pokusun.yt@gmail.com <pokusun.yt@gmail.com>
2024-02-25 18:47:03 +0100

    webui on sdk 8 works in container

--- BeerDispancer/BeerDispencer.WebUi/Program.cs
+++ BeerDispancer/BeerDispencer.WebUi/Program.cs
@@ -5,42 +5,45 @@ using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 using Radzen;
 
-
-
-var builder = WebAssemblyHostBuilder.CreateDefault(args);
-
-Uri webApiHost;
-
-
-if (builder.HostEnvironment.Environment == "Development")
+internal class Program
 {
-    webApiHost = new Uri("http://localhost:5268");
+    private static async Task Main(string[] args)
+    {
+        var builder = WebAssemblyHostBuilder.CreateDefault(args);
+
+        Uri webApiHost;
+
+
+        if (builder.HostEnvironment.Environment == "Development")
+        {
+            webApiHost = new Uri("http://localhost:5268");
+        }
+        else
+        {
+            webApiHost = new Uri("https://ugly-coyote.azurewebsites.net");
+        }
+
+        builder.RootComponents.Add<App>("#app");
+        builder.RootComponents.Add<HeadOutlet>("head::after");
+
+        builder.Services.AddScoped<NotificationService>();
+        builder.Services.AddScoped<DialogService>();
+
+        builder.Services.AddScoped<UserNotificationService>();
+        builder.Services.AddScoped<HttpRequestMessageHandler>();
+
+        builder.Services.AddSingleton<TimeZoneService>();
+        builder.Services.AddSingleton<ILocalStorage, LocalStorage>();
+        builder.Services.AddSingleton<AccountService>();
+
+        builder.Services.AddHttpClient("ServerAPI", client => client.BaseAddress = webApiHost)
+            .AddHttpMessageHandler<HttpRequestMessageHandler>();
+
+        builder.Services.AddRadzenComponents();
+
+        var host = builder.Build();
+        await host.Services.GetRequiredService<AccountService>().InitializeAsync();
+
+        await host.RunAsync();
+    }
 }
-else
-{
-    webApiHost = new Uri("https://ugly-coyote.azurewebsites.net");
-}
-
-builder.RootComponents.Add<BeerDispenser.WebUi.App>("#app");
-builder.RootComponents.Add<HeadOutlet>("head::after");
-
-builder.Services.AddScoped<NotificationService>();
-builder.Services.AddScoped<DialogService>();
-
-builder.Services.AddScoped<UserNotificationService>();
-builder.Services.AddScoped<HttpRequestMessageHandler>();
-
-builder.Services.AddSingleton<TimeZoneService>();
-builder.Services.AddSingleton<ILocalStorage, LocalStorage>();
-builder.Services.AddSingleton<AccountService>();
-
-builder.Services.AddHttpClient("ServerAPI", client => client.BaseAddress = webApiHost)
-    .AddHttpMessageHandler<HttpRequestMessageHandler>();
-
-builder.Services.AddRadzenComponents();
-
-var host = builder.Build();
-await host.Services.GetRequiredService<AccountService>().InitializeAsync();
-
-await host.RunAsync();
-
